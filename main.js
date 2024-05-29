80aaa234268b69f8d2fd84e475d958ff9e9d89dc
nathanparle <nathanparle@gmail.com>
2024-05-29 19:13:26 +0100

    etch a sketch created struggles over 30 x 30

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-const container = document.querySelector("container");
+const container = document.querySelector("#container");
+container.classList.add("container")
 const horizontal = [];
 const vertical = [];
-for (var i = 0; i <= 15; i++) {
-    var horiz = document.body.appendChild(document.createElement('div'));
-    horiz.classList.add("horiz");
-    horizontal.push(horiz);
-
-for (var j= 0; j< 15; j++){
-    var vert = document.body.appendChild(document.createElement('div'));
-    vert.classList.add("vert");
-    horizontal.push(vert);
+function choice(){let num = prompt("Enter the rows and columns required, below 100 ( anything over 30 kills the broswer tbh): ");
+    if( num > 100 ){ prompt("Below 100! : ")}
+    else { build(num)}
+}
+function build(x){
+for (var i = 1; i <= x; i++) {
+    var square = document.createElement('div');
+    square.classList.add("square");
+    container.appendChild(square);
+    horizontal.push(square);
     
+for (var j= 1   ; j<= x; j++){
+    var square2 = document.createElement('div');
+    square2.classList.add("square2");
+    square.appendChild(square2);
+    horizontal.push(square2);
+    let newColor = document.querySelectorAll(".square2");
+          for (const color of newColor) {
+            color.addEventListener("mouseover", () => {
+              color.style.backgroundColor = "grey";
+            });
+          }
+}
+}
 }
 
+const request = document.createElement("button");
+request.classList.add("request");
+document.body.appendChild(request);
+request.textContent = "Click to change size of grid" 
+request.addEventListener('click', (event) => choice());
 
 
-}
+
+
+
+build(30);
