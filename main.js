70992db7cdb904b8e4ff3566e8cf40f9e8094bad
nathanparle <nathanparle@gmail.com>
2024-05-30 19:24:52 +0100

    modified to use random color, 100x100 works!

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-const container = document.querySelector("#container");
-container.classList.add("container")
-const horizontal = [];
-const vertical = [];
-function choice(){let num = prompt("Enter the rows and columns required, below 100 ( anything over 30 kills the broswer tbh): ");
-    if( num > 100 ){ prompt("Below 100! : ")}
-    else { build(num)}
-}
-function build(x){
-for (var i = 1; i <= x; i++) {
-    var square = document.createElement('div');
-    square.classList.add("square");
-    container.appendChild(square);
-    horizontal.push(square);
+const container = document.querySelector(".container");
+const request = document.querySelector(".changeSize");
+
+request.addEventListener('click', (event) => { choice()});
+
+function choice(){
+    while (container.firstChild) 
+        { container.removeChild(container.firstChild);
+        }
     
-for (var j= 1   ; j<= x; j++){
-    var square2 = document.createElement('div');
-    square2.classList.add("square2");
-    square.appendChild(square2);
-    horizontal.push(square2);
-    let newColor = document.querySelectorAll(".square2");
-          for (const color of newColor) {
-            color.addEventListener("mouseover", () => {
-              color.style.backgroundColor = "grey";
-            });
+    let num = prompt("Enter the rows and columns required, between 8 and 100 : ");
+    if( num > 100 || num < 8){ alert("Between 8 and 100 please : "); choice();}
+    else { build(num);}
+}
+const  build = (x) => {
+for (var i = 0; i < x; i++) {
+    rows = document.createElement('div');
+    rows.classList.add("row");
+    container.appendChild(rows);
+    
+       
+for (var j= 0   ; j< x; j++){
+    columns = document.createElement('div');
+    columns.classList.add("column");
+    
+    
+    rows.appendChild(columns);
+    columns.addEventListener('mouseover', changeColour);
           }
 }
 }
-}
 
-const request = document.createElement("button");
-request.classList.add("request");
-document.body.appendChild(request);
-request.textContent = "Click to change size of grid" 
-request.addEventListener('click', (event) => choice());
+const changeColour = (event)=>{ event.target.style.backgroundColor =  '#' + Math.floor((Math.random() * 1000000) + 1);}
+
+
+build(16);
 
 
 
 
-
-build(30);
